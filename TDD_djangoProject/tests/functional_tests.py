3899216d22d259c41bddf76f0ee2415634979f89
ShionLeeee <cuishiang990314@163.com>
2021-01-30 17:38:08 +0800

    添加了新的测试内容

--- TDD_djangoProject/tests/functional_tests.py
+++ TDD_djangoProject/tests/functional_tests.py
@@ -11,7 +11,7 @@ class NewVisitorTest(unittest.TestCase):
         chromedriver = 'C:\\Users\\ShionLee\\chromedriver_win32\\chromedriver.exe'
         self.browser = webdriver.Chrome(chromedriver)
 
-    def teatDown(self):
+    def tearDown(self):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -44,19 +44,30 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table'
+            f'New to-do item did not appear in table. Contents were:\n{table.text}'
         )
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了"Use peacock feathers to make a fly"（使用孔雀羽毛做假蝇）
         # 伊迪斯做事很有条理
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
 
         # 伊迪斯想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL
         # 而且页面中有一些文字解说这个功能
+        self.fail('Finish the test!')
 
         # 她访问那个URL，发现她的待办事项列表还在
 
